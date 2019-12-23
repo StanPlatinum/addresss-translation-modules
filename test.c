eb1cdefe11c0a13b7098811c99a52fa15d5267d3
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-12-23 15:14:44 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,8 +1,12 @@
+#include "iostream"
+
 #include "v2p.h"
 
 int main()
 {
 	char *v;
-	v = 
+        ASSERT((v = (char *)mmap(0, ALLOC_SIZE * n, PROT_READ | PROT_WRITE, ALLOC_FLAG, -1, 0)) != MAP_FAILED);
+	cout << “v: ” << v << endl;
 	uint64_t p = v2p(v);
+	cout << “p: ” << p << endl;
 }
