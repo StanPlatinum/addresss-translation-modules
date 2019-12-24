b0572a4c08850be487856e1cfd27fab974dc09e9
root <lpjlwjlpjlwj@126.com>
2019-12-24 15:17:44 -0500

    v2p worked

--- test.c
+++ test.c
@@ -1,10 +1,6 @@
-#include "iostream"
-
 #include "v2p.h"
 
 #include <sys/mman.h>
-
-using namespace std;
 
 #define ALLOC_FLAG (MAP_PRIVATE | MAP_ANONYMOUS)
 
@@ -12,12 +8,12 @@ int main()
 {
 	int n = 1;
 	char *v;
-	//char *v = "test";
-	cout << "test begins" << endl;
+	printf("test begins\n");
         ASSERT((v = (char *)mmap(0, ALLOC_SIZE * n, PROT_READ | PROT_WRITE, ALLOC_FLAG, -1, 0)) != MAP_FAILED);
-	//memset(v, 0, ALLOC_SIZE * n);
-	cout << "v addr: " << &v << endl;
+	//Weijie: must add the following line
+	memset(v, 0, ALLOC_SIZE * n);
+	printf("v addr: %p\n", v);
 
 	uint64_t p = v2p(v);
-	cout << "p addr: " << &p << endl;
+	printf("p addr: %p\n", p);
 }
